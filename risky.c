ae6039c7d04b1cf57ffd8f651505177c8f2629c5
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-01-24 18:37:50 +0000

    Some more cleaning up and commenting.

--- risky.c
+++ risky.c
@@ -13,15 +13,20 @@
 
 int main(int argc, char const *argv[])
 {
+    /*
+     * This example code builds a RISKY instruction:
+     * ADD register 1 to register 7, store result in register 2.
+     */
     instruction_t instruction;
-    instruction.opcode = GRT;
-    instruction.rds_register = 0x2;
+    instruction.opcode = ADD;
+    instruction.primary = 0x2;
     instruction.operands.registers.a = 0x1;
     instruction.operands.registers.b = 0x7;
     printf("Instruction size is %lu bytes\n", sizeof(instruction));
     printf("Instruction operands make up %lu byte\n", sizeof(instruction.operands));
     printf(
         "Full instruction in raw format: 0x%01X%01X%01X%01X\n",
-        instruction.opcode, instruction.rds_register, instruction.operands.registers.a, instruction.operands.registers.b);
+        instruction.opcode, instruction.primary,
+        instruction.operands.registers.a, instruction.operands.registers.b);
     return 0;
 }
--- risky.h
+++ risky.h
@@ -18,31 +18,19 @@
 extern "C"{
 #endif
 
+    // raw instruction type (2 unsigned bytes)
+    typedef uint8_t instruction_raw_t[2];
+
     // instruction opcodes enum
     typedef enum {
-        ADD = 0x0U,
-        SUB = 0x1U,
-        MUL = 0x2U,
-        MOD = 0x3U,
-        AND = 0x4U,
-        NOT = 0x5U,
-        OR = 0x6U,
-        XOR = 0x7U,
-        SAV = 0x8U,
-        LOD = 0x9U,
-        COP = 0xAU,
-        SET = 0xBU,
-        JMP = 0xCU,
-        JIF = 0xDU,
-        EQU = 0xEU,
-        GRT = 0xFU
-    } instruction_opcode_enum_t;
+        ADD = 0x0U, SUB = 0x1U, MUL = 0x2U, MOD = 0x3U,
+        AND = 0x4U, NOT = 0x5U, OR = 0x6U,  XOR = 0x7U,
+        SAV = 0x8U, LOD = 0x9U, COP = 0xAU, SET = 0xBU,
+        JMP = 0xCU, JIF = 0xDU, EQU = 0xEU, GRT = 0xFU
+    } instruction_opcode_e;
 
     // instruction opcode type (restricted to 4 bits in struct)
     typedef uint8_t instruction_opcode_t;
-
-    // raw instruction type (2 unsigned bytes)
-    typedef uint8_t instruction_raw_t[2];
 
     // // register address type (restricted to 4 bits in struct)
     typedef uint8_t register_address_t;
@@ -55,22 +43,22 @@ extern "C"{
 
     // registers struct, used for packing two register operands into a byte
     typedef struct {
-        register_address_t a : 4;
-        register_address_t b : 4;
+        register_address_t a : 4; // restricted to 4 bits
+        register_address_t b : 4; // restricted to 4 bits
     } registers_t;
 
     // operands union, used for the second byte of an instruction
     typedef union instruction_operands_t {
-        memory_address_t memory_address;
-        literal_value_t literal_value;
-        registers_t registers;
+        memory_address_t memory_address; // an 8-bit memory address
+        literal_value_t literal_value; // an 8-bit literal value
+        registers_t registers; // two 4-bit registers
     } instruction_operands_t;
 
     // instruction type, used to represent a whole instruction
     typedef struct {
-        instruction_opcode_t opcode : 4;
-        register_address_t rds_register : 4;
-        instruction_operands_t operands; // 8 bits
+        instruction_opcode_t opcode : 4; // restricted to 4 bits
+        register_address_t primary : 4; // primary reg for result/destination/source
+        instruction_operands_t operands; // instruction_operands_t is 8 bits
     } instruction_t;
 
 #ifdef __cplusplus
