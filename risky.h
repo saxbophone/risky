ddfe80cbfecc076d0a6cf02b87ae2aa24bade5db
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-01-24 21:06:32 +0000

    Amend missed out changes

--- risky.h
+++ risky.h
@@ -61,10 +61,10 @@ extern "C"{
         instruction_operands_t operands; // instruction_operands_t is 8 bits
     } instruction_t;
 
-    // Returns the raw bytes of an instruction struct
+    // Populates a byte array with the raw bytes that represent an instruction struct
     void instruction_to_raw(instruction_t instruction, instruction_raw_t * raw_instruction);
 
-    // Creates a new instruction struct from raw bytes
+    // Creates and returns a new instruction struct from raw bytes
     instruction_t instruction_from_raw(instruction_raw_t * raw_instruction);
 
 #ifdef __cplusplus
