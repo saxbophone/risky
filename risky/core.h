6ce5f11044e4c9879a7e8215218fb261b86aa7f7
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2016-09-16 19:19:39 +0100

    Add blank core compilation unit and set up for unit test discovery

---
+++ risky/core.h
@@ -0,0 +1,23 @@
+/*
+ * core - this compilation unit defines the data types used for representing the
+ * state of the RISKY virtual machine, and some functions for creating and
+ * destroying objects of such types.
+ */
+#ifndef SAXBOPHONE_RISKY_CORE_H
+#define SAXBOPHONE_RISKY_CORE_H
+
+#include <stdint.h>
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+//
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+// end of header file
+#endif
