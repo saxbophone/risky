358c57b79f1512d989f7e836f28fcdd310eb9cac
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2016-09-16 18:50:51 +0100

    Add a function return status enum

--- risky/risky.h
+++ risky/risky.h
@@ -8,11 +8,20 @@
 extern "C"{
 #endif
 
+// struct for representing version of RISKY
 typedef struct version_t {
     uint8_t major;
     uint8_t minor;
     uint8_t patch;
 } version_t;
+
+// enum for storing information about the error status of a function
+typedef enum status_t {
+    UNKNOWN = 0,
+    MALLOC_REFUSED,
+    IMPOSSIBLE_CONDITION,
+    SUCCESS,
+} status_t;
 
 extern const version_t VERSION;
 
