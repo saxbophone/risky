a81d4e44077ccb8c55adad58aa476529bb0703ce
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2016-09-16 22:53:51 +0100

    Add instruction data type and all it's dependent data types Closes #12

--- risky/core.h
+++ risky/core.h
@@ -6,6 +6,7 @@
 #ifndef SAXBOPHONE_RISKY_CORE_H
 #define SAXBOPHONE_RISKY_CORE_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #include "risky.h"
@@ -27,6 +28,34 @@ typedef struct risky_vm_state_t {
     // a pointer to a dynamically allocated array of RAM (65,536 bytes / 64KiB)
     risky_ram_t * ram;
 } risky_vm_state_t;
+
+// all RISKY opcodes
+typedef enum risky_opcode_t {
+    NOP, JMP, BRA, HLT, // no-op, jump, branch, halt
+    EQU, NEQ, GTN, LTN, // equal?, not equal?, greater than?, less than?
+    ADD, SUB, MLT, DIV, // add, subtract, multiply, divide
+    MOD, INC, DEC, QOP, // modulo, increment, decrement, query last operation
+    EOR, AND, XOR, NOT, // either or, and, exclusive or, not
+    LSH, RSH, ROT, CAS, // left shift, right shift, rotate, cascade
+    SET, COP, LOD, SAV, // set register, copy register, load memory, save memory
+    QDC, CDC, REA, WRI, // query, configure, read from, write to (data channel)
+} risky_opcode_t;
+
+// register address type
+typedef uint8_t risky_register_address_t;
+// RAM address type
+typedef risky_register_t risky_ram_address_t;
+
+// risky instruction struct
+typedef struct risky_instruction_t {
+    risky_opcode_t opcode; // instruction opcode
+    // additional instruction flags
+    bool a_flag, b_flag, c_flag;
+    // 'return register' and other main operands
+    risky_register_address_t r, a, b;
+    // the other possible 16-bit memory address / literal value operand
+    risky_register_t l;
+} risky_instruction_t;
 
 /*
  * given a pointer to a risky_vm_state_t, initialises the state struct,
