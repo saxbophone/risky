a391384915c7b4d84a45d48c48d244743b9c099e
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2016-09-22 22:10:01 +0100

    Add MPL License 'Exhibit A' declarations to every source code file This is in keeping with the recommendations of Mozilla, to make it so there is no ambiguity of the license status of any file in the project.

--- fibonacci.asm
+++ fibonacci.asm
@@ -1,3 +1,9 @@
+; This Source Code Form is subject to the terms of the Mozilla Public
+; License, v. 2.0. If a copy of the MPL was not distributed with this
+; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;
+; Copyright (c) 2016, Joshua Saxby joshua.a.saxby+TNOPLuc8vM==@gmail.com
+
 ; a very simple assembly program for RISKY
 ; this program computes fibonacci values up to 100
 ; it outputs them on data channel 1
--- unit_test_harness/harness.h
+++ unit_test_harness/harness.h
@@ -1,4 +1,10 @@
 /*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2016, Joshua Saxby joshua.a.saxby+TNOPLuc8vM==@gmail.com
+ *
  * harness - this compilation unit provides a simple test harness to use for the
  * unit tests of RISKY
  */
